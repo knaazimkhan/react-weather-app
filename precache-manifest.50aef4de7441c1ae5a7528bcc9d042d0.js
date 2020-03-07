7fdf3b0ce0278d8c4b8ae78273519a0e6697b593
Naazim Khan <knaazimkhan@gmail.com>
2020-03-07 17:06:47 +0530

    Updates

---
+++ precache-manifest.50aef4de7441c1ae5a7528bcc9d042d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e071f5d77657a80d3556f8e06f160fea",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "398fe982b0f6ac5dd928",
+    "url": "/react-weather-app/static/css/main.c0df465c.chunk.css"
+  },
+  {
+    "revision": "86b66ee85ef6d402d647",
+    "url": "/react-weather-app/static/js/2.50adb46b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-weather-app/static/js/2.50adb46b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "398fe982b0f6ac5dd928",
+    "url": "/react-weather-app/static/js/main.7487403c.chunk.js"
+  },
+  {
+    "revision": "d2857d5f4a409e252419",
+    "url": "/react-weather-app/static/js/runtime-main.a75341be.js"
+  }
+]);
